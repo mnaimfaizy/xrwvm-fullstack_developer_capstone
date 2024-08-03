2f22c1cdbbce914e74657777e5200067ad217103
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-08-02 23:09:18 -0400

    chore: include the deployment and sentiment_analyzer urls

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
@@ -20,7 +19,6 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
@@ -28,7 +26,6 @@ class CarModel(models.Model):
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
